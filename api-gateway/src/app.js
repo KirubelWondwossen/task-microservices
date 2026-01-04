cf30e046cd00449757d14fd589d5e46ef1d5a28e
Kirubel Wondwossen <kirubelwondwossen30@gmail.com>
2026-01-04 15:37:52 +0300

    feat(auth-service): implement user registration and login with JWT authentication

--- api-gateway/src/app.js
+++ api-gateway/src/app.js
@@ -8,6 +8,16 @@ app.use(
   createProxyMiddleware({
     target: "http://localhost:4001",
     changeOrigin: true,
+    onProxyReq(proxyReq, req) {
+      if (req.body) {
+        const bodyData = JSON.stringify(req.body);
+
+        proxyReq.setHeader("Content-Type", "application/json");
+        proxyReq.setHeader("Content-Length", Buffer.byteLength(bodyData));
+
+        proxyReq.write(bodyData);
+      }
+    },
   })
 );
 
--- auth-service/src/app.js
+++ auth-service/src/app.js
@@ -1,12 +1,50 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
 const app = express();
 
+// 👇 MUST be here
 app.use(express.json());
 
+const users = [];
+
+// Register
+app.post("/auth/register", (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ message: "Missing fields" });
+  }
+
+  users.push({ username, password });
+  res.status(201).json({ message: "User registered" });
+});
+
+// Login
+app.post("/auth/login", (req, res) => {
+  const { username, password } = req.body;
+
+  const user = users.find(
+    (u) => u.username === username && u.password === password
+  );
+
+  if (!user) {
+    return res.status(401).json({ message: "Invalid credentials" });
+  }
+
+  const token = jwt.sign({ username }, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+  });
+
+  res.json({ token });
+});
+
+// Health
 app.get("/health", (req, res) => {
   res.json({ status: "Auth Service running" });
 });
 
 app.listen(4001, () => {
-  console.log("Auth Service on port 4001");
+  console.log("Auth Service running on port 4001");
 });
